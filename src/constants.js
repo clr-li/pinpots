2380211eb13fc6408dbe17bfc326cbc3e09a8d9a
clr-li <111320104+clr-li@users.noreply.github.com>
2024-09-12 16:15:06 -0700

    Production url

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ module.exports = {
   TOP_POST_LIKES_THRESHOLD: 1,
   // HOSTNAME: 'https://pinpot-service-191609172403.us-central1.run.app/',
 
-  HOSTNAME: "http://localhost:8080",
+  HOSTNAME: "https://pinpots.onrender.com/8080",
 };
