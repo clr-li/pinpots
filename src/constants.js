3aba5a4568e9faf8aa8b381e7a3b0b2c6e1682b4
clr-li <111320104+clr-li@users.noreply.github.com>
2024-09-13 09:14:46 -0700

    Fix typo

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,6 @@ module.exports = {
   TOP_POST_LIKES_THRESHOLD: 1,
   // HOSTNAME: 'https://pinpot-service-191609172403.us-central1.run.app/',
 
-  HOSTNAME: `https://pinpots.onrender.com}`,
+  HOSTNAME: `https://pinpots.onrender.com`,
   // HOSTNAME: 'http://localhost/8000',
 };
