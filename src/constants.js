bf13674b693dafe1254632ec15f9c7bc724b5c41
clr-li <111320104+clr-li@users.noreply.github.com>
2024-11-21 21:33:14 -0800

    WORKS: changed likes threshold

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOP_POST_LIKES_THRESHOLD: 1,
+  TOP_POST_LIKES_THRESHOLD: 3,
   // HOSTNAME: 'https://pinpot-service-191609172403.us-central1.run.app/',
 
   HOSTNAME: `https://pinpots.onrender.com`,
