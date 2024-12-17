cebb9a64880bba09b856ba95d164dbf6593af1f3
clr-li <111320104+clr-li@users.noreply.github.com>
2024-12-16 19:54:34 -0800

    Toggle between dev and prod

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,8 @@ module.exports = {
   TOP_POST_LIKES_THRESHOLD: 3,
   // HOSTNAME: 'https://pinpot-service-191609172403.us-central1.run.app/',
 
-  HOSTNAME: `https://pinpots.onrender.com`,
-  // HOSTNAME: 'http://localhost/8000',
+  HOSTNAME:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:8000'
+      : 'https://pinpots.onrender.com',
 };
