99a255e7934098b8eaaf1ecb0385797beb5d55e9
clr-li <111320104+clr-li@users.noreply.github.com>
2025-01-10 01:11:14 -0800

    added xcode ios project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pinpots.com',
+  appName: 'pinpots',
+  webDir: 'build'
+};
+
+export default config;
