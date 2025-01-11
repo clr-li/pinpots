f467b5e89b36b4b51f4a5c8bc41a624062c1511a
clr-li <111320104+clr-li@users.noreply.github.com>
2025-01-10 17:19:27 -0800

    Fixed ios nav

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.pinpots.app',
   appName: 'pinpots',
-  webDir: 'build'
+  webDir: 'build',
+  server: {
+    url: 'http://localhost:8000',
+    cleartext: true,
+  },
 };
 
 export default config;
